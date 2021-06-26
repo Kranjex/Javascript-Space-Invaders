e5324fcaa3f1b3c531980a4e9f9302e5739880eb
Vid Kranjec <84270214+Kranjex@users.noreply.github.com>
2021-06-26 12:56:02 +0200

    Add files via upload

--- Space-invaders/app.js
+++ Space-invaders/app.js
@@ -2,15 +2,34 @@
 const gameScreen = document.getElementById('gameScreen');
 const restartBtn = document.getElementById('restartButton');
 const spaceship = document.querySelector('.ship');
-// Game screen size
-const GAME_WIDTH = 1500;
-const GAME_HEIGHT = 900;
-// Invaders measurements
-const INVADER_WIDTH = 94;
-const INVADER_HEIGHT = 67;
-const INVADER_MARGIN = 10;
+// MEDIA QUERY
+var media = window.matchMedia('(max-width: 1920px)');
+if (media.matches) {
+  // Game screen measurements
+  var GAME_WIDTH = 750;
+  var GAME_HEIGHT = 450;
+  // Invader measurements
+  var INVADER_WIDTH = 47;
+  var INVADER_HEIGHT = 33.5;
+  var INVADER_MARGIN = 5;
+} else {
+  // Game screen measurements
+  var GAME_WIDTH = 1500;
+  var GAME_HEIGHT = 900;
+  // Invader measurements
+  var INVADER_WIDTH = 94;
+  var INVADER_HEIGHT = 67;
+  var INVADER_MARGIN = 10;
+}
 
 window.onload = () => {
+  console.log(
+    GAME_WIDTH,
+    GAME_HEIGHT,
+    INVADER_WIDTH,
+    INVADER_HEIGHT,
+    INVADER_MARGIN
+  );
   // init functions
   // Alien spawn function
 
@@ -53,20 +72,21 @@ window.onload = () => {
     switch (e.key) {
       // Moving in left direction
       case 'ArrowLeft':
-        if (parseInt(spaceship.style.left) % GAME_WIDTH !== 0) {
+        if (parseInt(style.left) % GAME_WIDTH !== 0) {
           style.left = `${parseInt(style.left) - 20}px`;
           //style.rotateY = 'rotateX(-45deg)';
-        } else if (parseInt(spaceship.style.left) === 0) {
+        } else if (parseInt(style.left) === 0) {
+          console.log('You hit left border');
           style.left = `${parseInt(style.left) + 20}px`;
         }
         //style.rotateY = 'rotateX(0deg)';
         break;
       // Moving in right direction
       case 'ArrowRight':
-        if (parseInt(spaceship.style.left) % GAME_WIDTH !== 0) {
+        if (parseInt(style.left) % GAME_WIDTH !== 0) {
           //style.rotateY = 'rotateX(45deg)';
           style.left = `${parseInt(style.left) + 20}px`;
-        } else if (parseInt(spaceship.style.left) === 1500) {
+        } else if (parseInt(style.left) === 1500) {
           style.left = `${parseInt(style.left) - 20}px`;
         }
         //style.transform = 'rotateX(0deg)';
